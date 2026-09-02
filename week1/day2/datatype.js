f9370786a4d70af93c0cf31acb988b54b06e5f17
Kamal2494 <Kamal240794@gmail.com>
2026-09-02 10:52:29 +0200

    Add files via upload
    week1

---
+++ week1/day2/datatype.js
@@ -0,0 +1,21 @@
+
+
+var firstName = "Kamal";
+console.log(firstName)
+console.log(typeof firstName)
+
+var companyName ="CTS";
+console.log(companyName)
+console.log(typeof companyName)
+
+var mobileNumber="12233";
+console.log(mobileNumber)
+console.log(typeof mobileNumber)
+
+var isAutomation=true;
+console.log(isAutomation);
+console.log(typeof isAutomation)
+
+var hasPlaywright;
+console.log(hasPlaywright);
+console.log(typeof hasPlaywright)
